8339d115d3767ec025c6570e90868f074f586367
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-04 13:45:56 +0000

    fnProfileAddWallBookmarkSelector

--- proxy.js
+++ proxy.js
@@ -1,12 +1,16 @@
 function fnOnLoad() {
 	//alert(window.location.pathname);
-	createBackButton();
+	fnCreateBackButton();
+	
+	if (window.location.pathname == "/en/ios/home/profile") {
+		fnProfile();
+	}
 }
 
-function createBackButton() { 
+function fnCreateBackButton() { 
 	var divTag = document.createElement("div"); 
 
-	divTag.id = "backButtonID"; 
+	divTag.id = "backButtonDiv"; 
 
 	divTag.style["z-index"] = 1000; 
 
@@ -15,6 +19,31 @@ function createBackButton() {
 	divTag.style.left = "280px"; 
 	divTag.style.top = "40px"; 
 
-	divTag.innerHTML = '<button class="sexybutton sexysmall sexysimple sexyblue" onClick="javascript:history.go(-1);">Back</button>'; 
+	divTag.innerHTML = '<button class="sexybutton sexysmall sexysimple sexyblue" onmousedown="javascript:history.go(-1);">Back</button>'; 
+	document.body.appendChild(divTag); 
+}
+
+function fnProfile() {
+	fnProfileAddWallBookmarkSelector();
+}
+
+function fnProfileAddWallBookmarkSelector() {
+	var divTag = document.createElement("div"); 
+
+	divTag.id = "wallBookmarkDiv"; 
+
+	divTag.style["z-index"] = 1000; 
+
+	divTag.style.position = "absolute"; 
+
+	divTag.style.left = "280px"; 
+	divTag.style.top = "80px"; 
+
+	var selectorHTML = '<select name=sel onchange="if (this.options[this.options.selectedIndex].value>0) window.location=""/en/ios/friends/profile?pid=""+(this.options[this.options.selectedIndex].value);"><option value="0">---';
+	selectorHTML+='<option value="2398072562">aafds';
+	selectorHTML+='<option value="1563407917">Item 1';
+	selectorHTML+='</select>'; 
+
+	divTag.innerHTML = selectorHTML;
 	document.body.appendChild(divTag); 
 }
