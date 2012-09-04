216ad7f1560159f98e6b029ab52e4edeb0d8ecf7
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-04 15:59:37 +0000

    added bookmark system

--- proxy.js
+++ proxy.js
@@ -47,8 +47,10 @@ function fnProfileAddWallBookmarkSelector() {
 	divTag.style.top = "100px"; 
 
 	var selectorHTML = '<select name="sel" onchange="window.location=\'/en/ios/friends/profile?pid=\'+this.options[this.options.selectedIndex].value;"><option selected value="0">Wall Bookmark</option>';
-	selectorHTML+='<option value="2398072562">abc</option>';
-	selectorHTML+='<option value="1563407917">cde</option>';
+	var aFriendArray = fnGetBookmarkFriendArray();
+	for (var i=0;i<aFriendArray.length;i++) {
+		selectorHTML+='<option value="' + aFriendArray[i].split(fnGetConnector())[0] + '">' + aFriendArray[i].split(fnGetConnector())[1] + '</option>';
+	}
 	selectorHTML+='</select>'; 
 
 	divTag.innerHTML = selectorHTML;
@@ -74,7 +76,7 @@ function fnProfileAddFriendWallBookmarkButtons() {
 	divTag.style.position = "absolute"; 
 	divTag.style.left = "100px"; 
 	divTag.style.top = "210px"; 
-	divTag.innerHTML = '<button class="sexybutton sexysmall sexysimple sexyblue" onmousedown="javascript:fnSetCookie(\'testVar\',\'hey\');alert(friendship.nickname);">Add</button>'; 
+	divTag.innerHTML = '<button class="sexybutton sexysmall sexysimple sexyblue" onmousedown="javascript:fnBookmarkFriend();">Add</button>'; 
 	document.body.appendChild(divTag);
 	
 	divTag = document.createElement("div"); 
@@ -83,8 +85,74 @@ function fnProfileAddFriendWallBookmarkButtons() {
 	divTag.style.position = "absolute"; 
 	divTag.style.left = "150px"; 
 	divTag.style.top = "250px"; 
-	divTag.innerHTML = '<button class="sexybutton sexysmall sexysimple sexyblue" onmousedown="javascript:alert(fnGetCookie(\'testVar\'));">Del</button>'; 
+	divTag.innerHTML = '<button class="sexybutton sexysmall sexysimple sexyblue" onmousedown="javascript:fnUnBookmarkFriend();">Del</button>'; 
 	document.body.appendChild(divTag);
+}
+
+function fnGetSeparator() {
+    var div, ta, text;
+
+    div = document.createElement("div");
+    div.innerHTML = "<textarea>one\ntwo</textarea>";
+    ta = div.firstChild;
+    text = ta.value;
+    return text.indexOf("\r") >= 0 ? "\r\n" : "\n";
+}
+
+function fnGetConnector() {
+    return "DS_5kyp3_Cl@n";
+}
+
+function fnBookmarkFriend() {
+	var aFriendArray = fnGetBookmarkFriendArray();
+	if (fnArrayHasItem(aFriendArray, friendship.pid + fnGetConnector() + friendship.nickname )) {
+	}
+	else {
+		aFriendArray.push(friendship.pid + fnGetConnector() + friendship.nickname);
+	}
+	aFriendArrayText = aFriendArray.join(fnGetSeparator());
+	fnSetCookie("friendBookmark",aFriendArrayText);
+}
+
+function fnUnBookmarkFriend() {
+	var aFriendArray = fnGetBookmarkFriendArray();
+	fnArrayRemoveItem(aFriendArray, friendship.pid + fnGetConnector() + friendship.nickname ));
+	aFriendArrayText = aFriendArray.join(fnGetSeparator());
+	fnSetCookie("friendBookmark",aFriendArrayText);
+}
+
+function fnGetBookmarkFriendArray() {
+	var aFriendArray;
+	var aFriendArrayText = fnGetCookie("friendBookmark");
+	if (aFriendArrayText == null) {
+		aFriendArray = [];
+	}
+	else {
+		aFriendArray = aFriendArrayText.split(fnGetSeparator());
+	}
+	return aFriendArray;
+}
+
+function fnArrayHasItem(originalArray, itemToDetect) {
+	var j = 0;
+	while (j < originalArray.length) {
+		if (originalArray[j] == itemToDetect) {
+			return true;
+		} else { j++; }		
+	}
+	return false;
+}
+
+function fnArrayRemoveItem(originalArray, itemToRemove) {
+	var j = 0;
+	while (j < originalArray.length) {
+	//	alert(originalArray[j]);
+		if (originalArray[j] == itemToRemove) {
+			originalArray.splice(j, 1);
+		} else { j++; }
+	}
+	//	assert('hi');
+	return originalArray;
 }
 
 function fnSetCookie(c_name,value)
@@ -109,4 +177,5 @@ function fnGetCookie(c_name)
 			return unescape(y);
 		}
 	}
+	return null;
 }
