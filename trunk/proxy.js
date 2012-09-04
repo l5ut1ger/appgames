108bf4243543c5887b34c48b6e5e5502ac1a707f
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-04 14:27:53 +0000

    debug bookmark

--- trunk/proxy.js
+++ trunk/proxy.js
@@ -39,9 +39,9 @@ function fnProfileAddWallBookmarkSelector() {
 	divTag.style.left = "180px"; 
 	divTag.style.top = "100px"; 
 
-	var selectorHTML = '<select name="sel" onchange="alert(this.options[this.options.selectedIndex].value);"><option selected value="0">---';
-	selectorHTML+='<option value="/en/ios/friends/profile?pid=2398072562">aafds';
-	selectorHTML+='<option value="/en/ios/friends/profile?pid=1563407917">Item 1';
+	var selectorHTML = '<select name="sel" onchange="alert(this.options[this.options.selectedIndex].value);"><option selected value="0">---</option>';
+	selectorHTML+='<option value="/en/ios/friends/profile?pid=2398072562">aafds</option>';
+	selectorHTML+='<option value="/en/ios/friends/profile?pid=1563407917">Item 1</option>';
 	selectorHTML+='</select>'; 
 
 	divTag.innerHTML = selectorHTML;
