c8d1eaf6fca05fa3e7bb4c84e8372ddcff752229
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-04 13:22:35 +0000

    create back button

--- proxy.js
+++ proxy.js
@@ -1,5 +1,20 @@
 function fnOnLoad() {
-	alert(window.location.pathname);
+	//alert(window.location.pathname);
+	createBackButton();
 }
 
+function createBackButton() { 
+	var divTag = document.createElement("div"); 
 
+	divTag.id = "backButtonID"; 
+
+	divTag.style["z-index"] = 1000; 
+
+	divTag.style.position = "absolute"; 
+
+	divTag.style.left = "280px"; 
+	divTag.style.top = "40px"; 
+
+	divTag.innerHTML = '<button class="sexybutton sexysmall sexysimple sexyblue" onClick="javascript:history.go(-1);">Back</button>'; 
+	document.body.appendChild(divTag); 
+}
