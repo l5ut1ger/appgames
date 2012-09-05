04cf1d0b2d04a4e79d185f6054209bafd888121a
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-05 17:12:35 +0000

    testing jquery notice

--- proxy_beta.js
+++ proxy_beta.js
@@ -210,10 +210,57 @@ function fnOnLoad() {
 	loadjscssfile("http://sexybuttons.googlecode.com/svn/trunk/sexybuttons.css", "css");
 	loadjscssfile("http://jquery-notice.googlecode.com/svn/trunk/jquery.notice.css", "css");	
 
-	loadjscssfile("http://jquery-notice.googlecode.com/svn/trunk/jquery.notice.js", "js");
+	jQuery.extend({			
+		noticeAdd: function(options)
+		{	
+			var defaults = {
+				inEffect: 			{opacity: 'show'},	// in effect
+				inEffectDuration: 	600,				// in effect duration in miliseconds
+				stayTime: 			3000,				// time in miliseconds before the item has to disappear
+				text: 				'',					// content of the item
+				stay: 				false,				// should the notice item stay or not?
+				type: 				'notice' 			// could also be error, succes
+			}
+			
+			// declare varaibles
+			var options, noticeWrapAll, noticeItemOuter, noticeItemInner, noticeItemClose;
+								
+			options 		= jQuery.extend({}, defaults, options);
+			noticeWrapAll	= (!jQuery('.notice-wrap').length) ? jQuery('<div></div>').addClass('notice-wrap').appendTo('body') : jQuery('.notice-wrap');
+			noticeItemOuter	= jQuery('<div></div>').addClass('notice-item-wrapper');
+			noticeItemInner	= jQuery('<div></div>').hide().addClass('notice-item ' + options.type).appendTo(noticeWrapAll).html('<p>'+options.text+'</p>').animate(options.inEffect, options.inEffectDuration).wrap(noticeItemOuter);
+			noticeItemClose	= jQuery('<div></div>').addClass('notice-item-close').prependTo(noticeItemInner).html('x').click(function() { jQuery.noticeRemove(noticeItemInner) });
+			
+			// hmmmz, zucht
+			if(navigator.userAgent.match(/MSIE 6/i)) 
+			{
+		    	noticeWrapAll.css({top: document.documentElement.scrollTop});
+		    }
+			
+			if(!options.stay)
+			{
+				setTimeout(function()
+				{
+					jQuery.noticeRemove(noticeItemInner);
+				},
+				options.stayTime);
+			}
+		},
+		
+		noticeRemove: function(obj)
+		{
+			obj.animate({opacity: '0'}, 600, function()
+			{
+				obj.parent().animate({height: '0px'}, 300, function()
+				{
+					obj.parent().remove();
+				});
+			});
+		}
+	});
 
 
-	//jQuery.noticeAdd({          text: 'This is a notification that does not stay',  stay: false        });
+	jQuery.noticeAdd({          text: 'This is a notification that does not stay',  stay: false        });
 
 	//alert(window.location.pathname);
 	fnCreateBackButton();
