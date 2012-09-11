ad47e189123b709f3aa382958317913632fff6f1
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-11 16:12:06 +0000

    debug

--- proxy_beta.js
+++ proxy_beta.js
@@ -216,7 +216,7 @@ function fnSpam(pID, pName, pMsg) {
 }
 */
 function fnProfileAddSpamButton() {
-	document.getElementById('div-bbs-form').innerHTML += '<a href="#"  class="btn __red __WS __HS" style="position:relative; top:0px; font-size:0.8em;">SPAM</a>';
+	document.getElementById('div-bbs-form').innerHTML += '<br/>';//<a href="#"  class="btn __red __WS __HS" style="position:relative; top:0px; font-size:0.8em;">SPAM</a>';
 	/*$('#btn-bbs-spam').click(function() { id="btn-bbs-spam"
 		var spamMsg = bbsBodyChanged ? $('#txt-bbs-body').val() : '';
 		var len = spamMsg.mblength();
