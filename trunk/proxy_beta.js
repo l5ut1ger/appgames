510b89ff53ec08c2ad9e0d17a3dc4fe49e041910
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-18 14:44:53 +0000

    cage use code

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -640,8 +640,8 @@ function fnFixMissionProcess() {
 
 		return false;
 	};
-	__effect_process = function(data) {};
-	__effect_cageSelect = function(data) {
+	EfectMng.efectList.process = __effect_process = function(data) {};
+	EfectMng.efectList.cageSelect = __effect_cageSelect = function(data) {
 		$.ajax_ex(false, '/en/ios/tower/cageUse', {'item_id' : 0, api : 'json',  '__hash' : ('' + (new Date()).getTime()) },function(result) {  $.redirect("\/en\/ios\/tower\/mission"); return;});
 	}
 }
