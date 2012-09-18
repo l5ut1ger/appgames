c7340575d9a17982bfcc096b17e0da8af96bbd4e
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-18 16:49:07 +0000

    boss Result

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -661,6 +661,18 @@ function fnTowerMission() {
 	}
 }
 
+// tower boss result
+
+function fnTowerBossResult() {
+	$.ajax_ex(false, '/en/ios/tower/bossGetResources', {choice : 1, '__hash' : ('' + (new Date()).getTime()) },function(result) {
+		if (result.payload.resources.foundType != null && result.payload.resources.foundType==10 && result.payload.resResult.items[result.payload.itemMaster.item_id].collected_count==6) { 
+			document.location="/en/ios/tower";
+		} else  {
+			document.location="/en/ios/tower/mission");
+		}
+	});
+}
+
 // battle
 
 function fnBattleBattle() {
@@ -700,6 +712,9 @@ function fnOnLoad() {
 	}
 	if (window.location.pathname === "/en/ios/tower/mission") {
 		fnTowerMission();
+	}
+	if (window.location.pathname === "/en/ios/tower/bossResult") {
+		fnTowerBossResult();
 	}
 	if (window.location.pathname === "/en/ios/battle/battle") {
 		fnBattleBattle();
