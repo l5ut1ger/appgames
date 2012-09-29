20961b3ca1b92f802dbb03d9dad6a5364f4cb5c1
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-29 14:29:47 +0000

    debug

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -1271,7 +1271,7 @@ function fnTowerFinalRanking() {
 // dungeon mission
 
 function fnDungeonMission() {
-	alert('ig.Game'+ig.Game);alert(ig.Game.save);
+	alert('ig.main'+ig.main);alert(ig.main.save);
 }
 
 // dungeon
