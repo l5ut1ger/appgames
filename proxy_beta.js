b1a55160e66a0a40ce8143cac880174cab5bccb6
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-09-29 14:51:52 +0000

    debug

--- proxy_beta.js
+++ proxy_beta.js
@@ -1271,6 +1271,7 @@ function fnTowerFinalRanking() {
 // dungeon mission
 
 function fnDungeonMission() {
+	alert('ig.game'+ig.game);alert(ig.game.save);
 	setTimeout(function(){alert('ig.game'+ig.game);alert(ig.game.save);}, 1000);
 }
 
