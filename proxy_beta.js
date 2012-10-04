fdb78e5de050a865018e970b741422c423ce9d2b
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-10-04 05:56:13 +0000

    debug

--- proxy_beta.js
+++ proxy_beta.js
@@ -1791,7 +1791,7 @@ function fnDungeonMission() {
 	if ((fnQueryString('go_next') == 'true' && dm.mission_count >= mMs.length)  || (document.referrer.startsWith("http://game.darksummoner.com/en/ios/dungeon/battle")) || (document.referrer.startsWith("http://game.darksummoner.com/en/ios/dungeon/win"))) {
 		if (fnDungeonProgTeam() != '' && fnDungeonImpulseTeam() != '' && fnDungeonCovertTeam() != '' && fnDungeonPsychoTeam() != '') {
 			fnDeckChangeAdvance(fnDungeonProgTeam(), false);
-			fnTimeOutRedirect('/en/ios/dungeon/battle?dungeon_tribe='+dm['dungeon_tribe']+'&area_id='+dm['area_id']);
+			fnTimeOutRedirect('/en/ios/dungeon/mission?dungeon_tribe='+dm['dungeon_tribe']+'&area_id='+dm['area_id']);
 			return;
 		}
 		fnTimeOutRedirect('/en/ios/dungeon');
