2366cc3d199157b1c9adba5b53643351619ce53c
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-10-06 10:18:14 +0000

    debug

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -2962,7 +2962,7 @@ function fnSlotInformationPreload() {
 }
 
 function fnSlotGame() {
-	alert('hi2 ' + $.ajax_ex);
+	alert('hi2 ' + now_date + " , " + confirm_id);
 	$.ajax_ex(false, "/en/ios/event/slotDecide", { now:now_date, cfmid:confirm_id }, function(data) {
 		alert('hi2');
 		fnRedirect("/en/ios/event/slotReward?now=" + now_date);
@@ -2970,11 +2970,6 @@ function fnSlotGame() {
 }
 
 function fnSlotGamePreload() {
-	alert('1 ' + $.ajax_ex);
-	$.ajax_ex(false, "/en/ios/event/slotDecide", { now:now_date, cfmid:confirm_id }, function(data) {
-		alert('hi2');
-		fnRedirect("/en/ios/event/slotReward?now=" + now_date);
-	}, function(){alert('success');}, function(){alert('error');});
 }
 
 // home
