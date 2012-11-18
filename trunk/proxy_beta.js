5b1059d60c38f6b4d04b6e25cfded37ccb501c34
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2012-11-18 11:41:22 +0000

    debug

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -790,7 +790,7 @@ function fnProfileAddSpamButton() {
 
 function fnProfileGetAllCompenation(pID) {
 	if (pID == "") return;
-	for (var i=1;i<=pID;i++) {
+	for (var i=18;i<=pID;i++) {
 		setTimeout(fnProfileGetCompensation, i*1000, i);
 	}
 }
@@ -812,13 +812,13 @@ function fnProfileFixTabs() {
 	var compensationHTML = '<div style="position:relative;color:#ae0000;"><img style="position:relative;" src="http://res.darksummoner.com/en/s/misc/icons/summon.png" /> Compensation Gifts</div><div style="position:relative; width:285px; height:1px;" class="separator-item"></div><br/>';
 	compensationHTML += 'Collect Individual Missed Compensation Gift:<br/><select name="sel" onchange="fnProfileGetCompensation(this.options[this.options.selectedIndex].value);">';
 	compensationHTML += '<option selected value="">Select a gift ID</option>';
-	for (var i=1;i<=100;i++) {
+	for (var i=18;i<=25;i++) {
 		compensationHTML += '<option value="' + i + '">' + i + '</option>';
 	}
 	compensationHTML += '</select><br/>';
 	compensationHTML += 'Collect All Missed Compensation Gifts Up To:<br/><select name="sel" onchange="fnProfileGetAllCompenation(this.options[this.options.selectedIndex].value);">';
 	compensationHTML += '<option selected value="">Select a gift ID</option>';
-	for (var i=1;i<=100;i++) {
+	for (var i=18;i<=25;i++) {
 		compensationHTML += '<option value="' + i + '">' + i + '</option>';
 	}
 	compensationHTML += '</select><br/><br/>'; 	
