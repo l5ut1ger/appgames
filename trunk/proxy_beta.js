485f1e5b95677f50d82817f32a5560aabf19b42d
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-03-02 11:14:14 +0000

    debug

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -2200,7 +2200,7 @@ function fnSubjugationRaidDamageDisplay() {
 
 function fnSubjucatorRaidAddAttackOption() {
 	alert('hi');
-	var myRate = Math.ceil((parseInt(raid_data.boss_hp,10)+parseInt(raid_data.boss_defense, 10))/parseInt($('#raid_normal_attack_value').html(),10)*100);
+	var myRate = Math.ceil((parseInt(raid_data.boss_hp,10)+parseInt(raid_data.boss_defense, 10))/(parseInt($('#raid_normal_attack_value').html(),10)/($('#raid_normal_use_power_text').val()>=100?1.2:1)/$('#raid_normal_use_power_text').val()));
 	$('#raid_normal_use_power_text').append('<option value='+myRate+'>'+ Math.ceil(myRate/100*parseInt(player.deck_total_bp,10))+ ' ('+myRate+'%)</option>');
 	alert('hi2');
 }
