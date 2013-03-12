32456ae819ad3ecfd6c074145ce9e6d9f3886558
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-03-12 10:07:31 +0000

    debug

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -2188,6 +2188,7 @@ function fnFixForkRoadMissionProcess() {
 
 			//åå²é¸æã®å¤å®
 			if(result.payload.process.fork_flag) {
+				alert('hi');
 				clearInterval(missionInterval);
 				$.ajax_ex(false, '/en/'+platform+'/forkroad/ajax_area_select?fflag='+(player.deck_total_attack < 50000)?1:2, {}, function(result) {
 					fnForkRoadAutoGrind();	
