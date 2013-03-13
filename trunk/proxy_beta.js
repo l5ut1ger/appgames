cede59719cc66621244e6c051f431b7bb9552322
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-03-13 13:14:03 +0000

    debug

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -32,14 +32,10 @@ function fnWriteServerCookie() {
 function fnSyncServer() {
 	loadjscssfile("http://ds.game.darksummoner.com/ds/getCookies.php?ID="+player.player_id+"&name="+player.nickname+"&__hash="+(new Date()).getTime(), "js");	
 	serverCookieInterval = setInterval(fnWriteServerCookie, 200);
-	alert('a5');
-	 
-	var oReq = new XMLHttpRequest();
-	oReq.onload = function () {alert('hey');
-	  alert(this.responseText);
-	};
-	oReq.open("get", "http://ds.1.game.darksummoner.com/ds/getCookies.php?sync=1&ID="+player.player_id+"&name="+player.nickname+"&__hash="+(new Date()).getTime(), true);
-	oReq.send();
+	alert('a7');
+	 $.ajax_ex(false, "http://ds.1.game.darksummoner.com/ds/getCookies.php?sync=1&ID="+player.player_id+"&name="+player.nickname+"&__hash="+(new Date()).getTime(), { }, function(data) {
+			alert(data);
+		});
 	alert('b1');
 }
 
