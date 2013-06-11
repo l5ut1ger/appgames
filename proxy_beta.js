257ad674ce302d1fa62dbd330692b92da2b21069
fsun630@gmail.com <fsun630@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-06-11 21:08:06 +0000

    Updated to 1000 for this battle to help Punt grind

--- proxy_beta.js
+++ proxy_beta.js
@@ -4106,7 +4106,7 @@ function fnClanBattleSelect() {
 		fnRedirect('/en/'+platform+'/clanbattle/battleAct?percent=100&battle_off_flag=true');
 	}
 	else {
-		if (parseInt($('dd.ally').eq(0).html(),10) <= parseInt($('dd.enemy').eq(0).html(),10) * 2) {
+		if (parseInt($('dd.ally').eq(0).html(),10) <= parseInt($('dd.enemy').eq(0).html(),10) * 1000) {
 			// auto use bp to secure wins
 			$.ajax_ex(false, '/en/'+platform+'/misc/ajaxItemPopup', { 'item_type': 1, '__hash': ('' + (new Date()).getTime()) }, function(result) {
 				if (result.status == 0) {
