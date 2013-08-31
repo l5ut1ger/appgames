16611f5c60651b5640ed89bd303fff83c2dfcd72
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-08-31 10:35:52 +0000

    debug

--- proxy_beta.js
+++ proxy_beta.js
@@ -1089,16 +1089,7 @@ function fnProfileAddSpamButton() {
 	document.getElementById('div-bbs-form').appendChild(divTag);
 
 	$('#btn-bbs-clearAll').click(function() { 
-		for (i=0;i<$( "div[id|='div-bbs-item-']" ).length;i++) {
-			$.getJSON('/en/ios/bbs/remove', {
-				'target_id': player.player_id,
-				'sub_id': $( "div[id|='div-bbs-item-']" ).eq(i).attr("id").replace("div-bbs-item-","");
-			}, function(result) {
-			});
-		}
-		while ($( "div[id|='div-bbs-item-']" ).length) {
-			$( "div[id|='div-bbs-item-']" ).eq(0).remove();
-		}	
+
 	});
 }
 
