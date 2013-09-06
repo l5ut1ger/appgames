5c092506c68a8b765f7b82a87d4c2de15e3e9ce0
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-09-06 18:14:11 +0000

    debug

--- trunk/proxy_beta.js
+++ trunk/proxy_beta.js
@@ -5719,16 +5719,9 @@ function fnAutoTradeMonster(pMonster, pURL) {
 						paramArr.give = new Object();
 						paramArr.give.type = 2;
 						paramArr.give.id=pMonster.unique_no;
-						paramArr.give.amount = "1&wt_1_1=3&wi_1_1=3001&wa_1_1="+tradePrice+"&wt_2_1=3&wi_2_1=3003&wa_2_1="+ Math.ceil(((Math.random() * 2) + 2)*tradePrice);alert('a');
-						//fnSetAutoRedirect(pURL);alert('b');
-						$(window).bind('beforeunload', function(){
-						    return false;
-						});
-						procDecision();						alert('c');
-						$(window).bind('beforeunload', function(){
-						    return true;
-						});
-						fnRedirect(pURL);
+						paramArr.give.amount = "1&wt_1_1=3&wi_1_1=3001&wa_1_1="+tradePrice+"&wt_2_1=3&wi_2_1=3003&wa_2_1="+ Math.ceil(((Math.random() * 2) + 2)*tradePrice);
+						fnSetAutoRedirect(pURL);
+						procDecision();
 					}
 				});
 			}
