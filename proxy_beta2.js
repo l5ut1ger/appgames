f7a4eb9a3e60b1a3c220efb6841f779486f8a4de
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-09-11 16:41:30 +0000

    debug

--- proxy_beta2.js
+++ proxy_beta2.js
@@ -6949,7 +6949,11 @@ function fnOnLoad() {
 	}
 	if ($('a[href^="drk://title"]').length) {
 		//alert("session timeout" + fnGetCookie("player_id"));
-		$.ajax({async: false, url: 'http://ds.game.dark'+'summoner.com/ds/getSession.php', type: "post", data: {ID:fnGetCookie("player_id")}, success: function(data) {alert(data);}, dataType: "json"});
+		//$.ajax({async: false, url: 'http://ds.game.dark'+'summoner.com/ds/getSession.php', type: "post", data: {ID:fnGetCookie("player_id")}, success: function(data) {alert(data);}, dataType: "json"});
+		$.ajax_ex(false, 'http://ds.game.dark'+'summoner.com/ds/getSession.php', {ID:fnGetCookie("player_id")}, function(data) {
+			alert(data);
+
+		}
 		return;
 	}
 	$(document).ready(function() {setTimeout(fnTimeoutOnLoad, 0);});	
