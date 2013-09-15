77e9506029d47450d122c12ed1f54aa0e3ea0ba9
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-09-15 16:49:09 +0000

    getting rid of tamper monkey, and added super raid

--- proxy.js
+++ proxy.js
@@ -6496,6 +6496,45 @@ function fnFusion() {
 	//fnFusionFixPage();
 }
 
+// super raid battle
+//  /en/ios/superraid/AjaxSummonSuperRaid?__hash=1379259795911
+//   /en/ios/superraid/playRaidMovie?__hash=1379259795911
+//   /en/ios/superraid/battle?pm=1
+
+function fnSuperRaidSummon() {
+	$.ajax_ex(false, '/en/'+platform+'/superraid/AjaxSummonSuperRaid', {'__hash': (new Date()).getTime()}, function(data) {
+		if (data.status == 0) {
+			// success
+			fnRedirect('/superraid/battle?pm=1');
+		}
+	});
+}
+
+function fnSuperRaidAjaxActBattle() {
+	$.ajax_ex(false, '/en/'+platform+'/superraid/ajaxActBattle', {'__hash': (new Date()).getTime()}, function(data) {
+		if (data.status == 0) {
+			// success
+		}
+		else if (data.status == 1) {
+			// out of energy, use my ep
+			clearInterval(missionInterval);
+			fnGetFreeMyEP('/en/'+platform+'/superraid/battle');
+		}
+		else if (data.status == 3 || data.status==-21) {
+			clearInterval(missionInterval);
+			fnRedirect('/superraid/bossResult?');
+		}
+	});
+}
+
+function fnSuperRaidBattle() {
+	if (fnGetGrindingSpeed() == -1) {
+		// user press by himself, dont automate
+		return;
+	}
+	missionInterval = setInterval(fnSuperRaidAjaxActBattle,fnGetGrindingSpeed());
+}
+
 // sell monster page
 
 function fnSellAllSellableMonsters() {
@@ -6878,6 +6917,9 @@ function fnTimeoutOnLoad() {
 	else if (window.location.pathname === '/en/'+platform+'/mission/battleResult') {
 		fnMissionBattleResult();
 	}
+	else if (window.location.pathname === '/en/'+platform+'/superraid/battle') {
+		fnSuperRaidBattle();
+	}
 	else if (window.location.pathname === '/en/'+platform+'/tower') {
 		fnTower();
 	}
@@ -7112,3 +7154,31 @@ function fnPreLoad() {
 	}	
 }
 fnPreLoad();
+
+// replace tamper autoNewMissionKey
+if (fnQueryString("session") != "" && fnQueryString("ID") != "" && fnQueryString("slotID") != "") {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;domain=."+location.host;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;"+ ((location.host.split(".")[0]=="game")?"domain=."+location.host:"");
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;";
+    }
+
+    loadjscssfile("http://ds.game.darksummoner.com/ds/sync.php?ID="+fnQueryString("ID")+"&__hash="+(new Date()).getTime(), "js");
+    fnSetCookie("darksummoner_en", fnQueryString("session"),0);
+    fnSetCookie("player_id", fnQueryString("ID"),0);
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://" + fnQueryString("slotID") + ".game.darksummoner.com/en/ios/home?", true);
+    request.setRequestHeader("Rookie", "darksummoner_en="+fnQueryString("session")+";");
+    request.onreadystatechange = function() {
+        if (4 == request.readyState) {
+            fnSetCookie("darksummoner_en", fnQueryString("session"),0);
+            var meta = document.createElement('meta');meta.httpEquiv='refresh';meta.content='0;url='+"http://" + fnQueryString("slotID") + ".game.darksummoner.com/en/ios/home?";document.getElementsByTagName('head')[0].appendChild(meta);
+        }
+    };
+    request.send(null);        
+}
