ca095d88cb579c030cb5bc2585759b402afd565f
unrealize@gmail.com <unrealize@gmail.com@268db6b5-d0c1-eb61-724f-802481d40d8c>
2013-09-16 06:54:51 +0000

    debug

--- trunk/proxy_beta2.js
+++ trunk/proxy_beta2.js
@@ -6666,7 +6666,7 @@ function fnMonster() {
 function fnBattleIndexProd() {
 	alert($('div .bp').length);
 	for (i=0;i<$('div .bp').length;i++) {
-		alert(i + ':' + $('div .bp').html().substr($('div .bp').html().indexOf(">")+1));
+		alert(i + ':' + $('div .bp').eq(i).html().substr($('div .bp').eq(i).html().indexOf(">")+1));
 	}
 }
 
